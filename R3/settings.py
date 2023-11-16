f0a66d5cb5317c40dc802c0991b7e7558a6a2e5e
Candyflip <candyflip@bk.ru>
2023-11-16 20:10:17 +0300

    add templates index.html and about.html and hide app_key

--- R3/settings.py
+++ R3/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open('app_key.txt', 'r') as f:
+with open('/home/candyflip/files/keys_and_tokkens/app_key.txt', 'r') as f:
     s_key = f.read().strip()
 
 # Quick-start development settings - unsuitable for production
