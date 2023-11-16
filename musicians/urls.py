e05049daf1317766c64fa3154d3bec047ca4ba06
Candyflip <candyflip@bk.ru>
2023-11-16 19:34:34 +0300

    add templates index.html and about.html

--- musicians/urls.py
+++ musicians/urls.py
@@ -6,8 +6,9 @@ register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index),  # http://127.0.0.1:8000/
-    path('events/', views.events),  # http://127.0.0.1:8000/events
-    path('categories/<int:cat_id>/', views.categories),  # http://127.0.0.1:8000/categories/2/
-    path('categories/<slug:cat_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/categories/slug2/
-    path('archive/<year4:year>/', views.archive),  # http://127.0.0.1:8000/archive/slug2/
+    path('events/', views.events, name='events'),  # http://127.0.0.1:8000/events
+    path('categories/<int:cat_id>/', views.categories_id, name='categories_id'),  # http://127.0.0.1:8000/categories/2/
+    path('categories/<slug:cat_slug>/', views.categories, name='categories'),  # http://127.0.0.1:8000/categories/slug2/
+    path('archive/<year4:year>/', views.archive, name='archive'),  # http://127.0.0.1:8000/archive/slug2/
+    path('about/', views.about, name='about')  # http://127.0.0.1:8000/about
 ]
--- musicians/views.py
+++ musicians/views.py
@@ -1,27 +1,45 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from datetime import datetime
+
+from django.template.loader import render_to_string
+from django.urls import reverse
+
+menu = ['About', 'Create new article', 'Contact us', 'Log in/Sing up']
 
 
 def index(request):  # HttpRequest
-    return HttpResponse("Musician's app page")
+    data = {
+        'title': 'Main page',
+        'menu': menu,
+    }
+    template = render_to_string('musicians/index.html', context=data)
+    return HttpResponse(template)
+
+
+def about(request):
+    data = {
+        'title': 'About us'
+    }
+    return render(request, 'musicians/about.html', context=data)
 
 
 def events(request):
     return HttpResponse('<h1> Events preview and photos</h1>')
 
 
-def categories(request, cat_id):
+def categories_id(request, cat_id):
     return HttpResponse(f'<h1>Categories</h1><p>category_id: {cat_id}</p>')
 
 
-def categories_by_slug(request, cat_slug):
+def categories(request, cat_slug):
     return HttpResponse(f'<h1>Categories</h1><p>category_by_slug: {cat_slug}</p>')
 
 
 def archive(request, year):
     if year > datetime.now().year:
-        raise Http404()
+        uri = reverse('events')
+        return redirect(uri)
     return HttpResponse(f'<h1>Events Archive</h1><p>Year: {year}</p>')
 
 
